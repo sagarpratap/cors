61fef91347d9cc4fc9360f0589744cb0dcd586e4
Sagar Pratap Singh <sagarpratapsingh15@gmail.com>
2022-05-21 02:57:36 +0800

    added cors enabled support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
 function App() {
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/cors', {mode:'cors'});
+      const data = await response.json();
+      console.log({ data })
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    makeAPICall();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Cors Guide</h1>
     </div>
   );
 }
-
 export default App;
